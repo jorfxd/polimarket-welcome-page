292d06f225a68f6b6f0016552ca499d540690c6f
Jahir124 <jahirmp13@gmail.com>
2025-12-07 17:53:38 -0500

    Small change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === "production" ? "/polimarket-welcome-page/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
