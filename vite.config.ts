3f23ff0f8bd9a6677e35acfd93a5313664091b92
jorfxd <jaarroba@espol.edu.ec>
2025-12-07 15:10:55 -0500

    desplegar

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/polimarket-welcome-page/",
   server: {
     host: "::",
     port: 8080,
